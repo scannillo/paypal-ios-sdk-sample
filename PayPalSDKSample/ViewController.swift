ec40e4e7a080fe522c9e7dd56121e09d8f24924c
Sammy Cannillo <scannillo@paypal.com>
2022-09-01 10:27:28 -0500

    Add initial/blank Xcode project
    Signed-off-by: Matt Wylder <mwylder@paypal.com>

---
+++ PayPalSDKSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PayPalSDKSample
+//
+//  Created by Samantha Cannillo on 9/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
